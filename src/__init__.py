ede5c93cf42812cada5b6d0c2d550a8c3971429a
MarecGents <marecgents_tao@sina.com>
2026-01-19 15:10:49 +0800

    [2026, 1, 19] update

--- src/__init__.py
+++ src/__init__.py
@@ -14,12 +14,12 @@ HEADERS = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
 }
 BODY = {
-	"query": "query{\n  items(lang:zh){\n    id\n    name\n    shortName\n    basePrice\n    avg24hPrice    \n    low24hPrice\n    high24hPrice\n    sellFor{ \n      vendor{\n        name\n        __typename\n        }\n      price\n      currency\n      priceRUB\n    }\n    buyFor{ \n      vendor{\n        name\n        __typename\n      }\n      price\n      currency\n      priceRUB\n    }\n    fleaMarketFee\n  }\n}"
+	"query": "query{\n  items(lang:zh){\n    id\n    name\n    shortName\n    basePrice\n    avg24hPrice    \n    low24hPrice\n    high24hPrice\n  }\n}"
 }
 
 #path value
 
-EXE_PATH = PathControl.locateGivenFolderNamePathNearby(PathControl.getParentsFolderPath(__file__, 2), "Sync-Online-FleaMarket", [])
+EXE_PATH = PathControl.locateGivenFolderNamePathNearby(PathControl.getParentsFolderPath(__file__, 2), "MGFleaMarket", [])
 # EXE_PATH = PathControl.getNowFolderPath(str(pathlib.Path().absolute()))
 GIT_PWSH_PATH = EXE_PATH
 APP_ROOT_PATH = EXE_PATH
@@ -27,31 +27,43 @@ APP_ROOT_PATH = EXE_PATH
 def priceCreat(base_info, itemJson):
 	pricesJson = {}
 	for it in base_info:
-		if len(it['buyFor']) == 0:
-			continue
-			pass
-		v = 0
-		for it2 in it["buyFor"]:
-			if it2["vendor"]['name'] == "跳蚤市场":
-				v = 1
-				pass
-			pass
-		if v != 1:
-			continue
-			pass
 		itemId = it["id"]
 		if itemId not in itemJson:
 			continue
-			pass
-		avg = it["avg24hPrice"]
-		base = it["basePrice"]
-		if not avg and base:
-			pricesJson[itemId] = base
-			continue
-			pass
-		pricesJson[itemId] = avg
+		priceGet = {
+			"base" : it["basePrice"],
+			"avg" : it["avg24hPrice"],
+			"low" : it["low24hPrice"],
+			"high" : it["high24hPrice"]
+		}
+		price = caculatePrice(priceGet)
+		pricesJson[itemId] = price
 		pass
 	return pricesJson
+
+baseWeight = 0.1
+def caculatePrice(priceGet):
+	"""
+	   计算加权价格 P
+	   :param base_weight_full: 当四项全有时，basePrice 的权重（建议 0.1～0.2）
+	   """
+
+	# 情况1: 只有 base
+	if priceGet["avg"] is None:
+		return priceGet["base"]
+
+	# 情况2: 有 base + avg24，但缺少 low/high
+	if priceGet["low"] is None or priceGet["high"] is None:
+		# 可自定义此场景的权重，例如 30% base + 70% avg
+		return 0.15 * priceGet["base"] + 0.85 * priceGet["avg"]
+
+	# 情况3: 四项全有
+	y = baseWeight
+	x = (1 - y) / (80 * y)
+
+	avg_part = 9 * x * priceGet["avg"]
+	range_part = x * (priceGet["low"] + priceGet["high"]) / 2.0
+	return y * priceGet["base"] + 8 * y * (avg_part + range_part)
 
 def priceJsonRebuild(priceJson, date):
 	return {
@@ -82,6 +94,7 @@ if __name__ == "__main__":
 		"date":_date.get_Date(),
 		"items":baseInfo
 	}
+
 	ITEMS_BASE_INFO_PATH = _path.combineFolderPathWithRelativePath(APP_ROOT_PATH,static_value.ITEMS_BASE_INFO)
 	ITEMS_PATH = _path.combineFolderPathWithRelativePath(APP_ROOT_PATH,static_value.ITEMS)
 	PRICE_PATH = _path.combineFolderPathWithRelativePath(APP_ROOT_PATH,static_value.PRICE)
